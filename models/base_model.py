2c04665d19fa73fcbb8441961f1c638fdf3bb846
Doouh <ake.giraldo@hotmail.com>
2020-03-31 02:02:12 +0000

    Fix to_dict()

--- models/base_model.py
+++ models/base_model.py
@@ -74,6 +74,6 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
-        if my_dict["_sa_instance_state"]:
-            del(my_dict["_sa_instance_state"])
+        if "_sa_instance_state" in my_dict:
+            my_dict.pop("_sa_instance_state")
         return my_dict
--- models/state.py
+++ models/state.py
@@ -15,5 +15,5 @@ class State(BaseModel, Base):
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
         cities = relationship("City",  backref='state', cascade='delete')
-    elif(environ.get("HBNB_TYPE_STORAGE") == "file"):
-        name = "" 
+    else:
+        name = ""
