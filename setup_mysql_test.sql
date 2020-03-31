6d0704b981243dfe4d01ccb1dee3f2dd49e4a324
Doouh <ake.giraldo@hotmail.com>
2020-03-31 01:02:24 +0000

    Back to another commit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,7 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 USE hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
--- SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
-UPDATE mysql.user SET authentication_string=PASSWORD('hbnb_test_pwd') WHERE User='hbnb_test';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
