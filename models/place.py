dcecf8d1e4d1b7453bdcbe13be822828fe0213af
pafuentess <pafuentess@unal.edu.co>
2020-04-01 11:13:26 -0500

    fix relationship in place

--- models/place.py
+++ models/place.py
@@ -39,7 +39,7 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship("Review", backref='place', cascade='delete')
+        reviews = relationship("Review", backref='place', cascade='all, delete')
         amenities = relationship("Amenity",  secondary=place_amenity, back_populates="place_amenities", viewonly=False)
     else:
         city_id = ""
