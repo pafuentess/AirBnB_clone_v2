9c71aaa76e861cb957076626dd8619733aca2d20
Doouh <ake.giraldo@hotmail.com>
2020-04-01 00:39:31 +0000

    Add metadata

--- models/place.py
+++ models/place.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 import os
 
 metadata = Base.metadata
-place_amenity = Table('place_amenity', Base.metadata,
+place_amenity = Table('place_amenity', metadata,
                       Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
                       Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True))
 
