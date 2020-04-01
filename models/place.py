533f6c32fa9c3b64eec2ca4b6b0f385d19404857
pafuentess <pafuentess@unal.edu.co>
2020-04-01 11:15:09 -0500

    fix relationship in place

--- models/place.py
+++ models/place.py
@@ -42,18 +42,6 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", backref='place', cascade='all, delete')
         amenities = relationship("Amenity",  secondary=place_amenity, back_populates="place_amenities", viewonly=False)
     else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
-
         @property
         def reviews(self):
             review = models.storage.all(Review)
