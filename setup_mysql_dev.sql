5f636c8f73a8c72fcbef5d39052c7ebbf454fc46
Doouh <ake.giraldo@hotmail.com>
2020-04-01 12:27:38 -0500

    Add a comemt to each line of queries

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,13 @@
 -- script that prepares a MySQL server for the project:
-
+-- Create a hbnb_dev_db if this doesn't exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Change de current database to hbnb_dev_db
 USE hbnb_dev_db;
+-- Create a new user if this doesn't exists
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+-- Set a password for last user created
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+-- Set all privileges for the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- I don't understand this line
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,13 @@
 -- script that prepares a MySQL server for the project:
-
+-- Create a hbnb_test_db if this doesn't exists
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Change de current database to hbnb_test_db
 USE hbnb_test_db;
+-- Create a new user if this doesn't exists
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+-- Set a password for last user created
 SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+-- Set all privileges for the user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- I don't understand this line
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
