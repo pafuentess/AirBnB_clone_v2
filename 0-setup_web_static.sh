6e6b3cca0cf34bc3f15b8cf01e54f6a069c09af9
pafuentess <pafuentess@unal.edu.co>
2020-04-09 16:14:11 -0500

    fix ortography

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 #sets up your web servers for the deployment of web_static
 
-apt-get update
-apt-get -y install nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo -e "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+sudo apt-get update
+sudo apt-get -y install nginx
+mkdir /data/ 
+mkdir /data/web_static/
+mkdir /data/web_static/releases/
+mkdir /data/web_static/shared/
+mkdir /data/web_static/releases/test/
+echo -e "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '42i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-service nginx restart
+sudo service nginx restart
 
