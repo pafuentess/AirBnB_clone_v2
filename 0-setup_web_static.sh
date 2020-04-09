e7b51b3b77f72905855dd62b01c497f6a2329469
pafuentess <pafuentess@unal.edu.co>
2020-04-09 15:13:13 -0500

    task 1

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#sets up your web servers for the deployment of web_static
+
+apt-get update
+apt-get -y install nginx
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+service nginx restart
+
