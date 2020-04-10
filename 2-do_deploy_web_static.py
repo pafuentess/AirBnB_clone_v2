56edad26cb05bda1279b48dd3adb6c054a2cd8c9
pafuentess <pafuentess@unal.edu.co>
2020-04-09 21:05:54 -0500

    task2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""doc"""
+
+import os
+import re
+from fabric.api import *
+from fabric.operations import run, put, sudo
+
+env.hosts = ['104.196.123.176', '54.91.99.170']
+
+
+def do_deploy(archive_path):
+    """
+        using fabric to distribute archive
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        stat(archive_path)
+    except:
+        return False
+    try:
+        name = archive_path.split("/")
+        name = name[1].split(".")
+        name = name[0]
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{}.tgz -C \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm /tmp/{}.tgz".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(name))
+        print("New version deployed!")
+        return True
+    except:
+        return False
