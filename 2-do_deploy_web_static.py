839c12caaa76a615550d6c445e76ed256aebe719
pafuentess <pafuentess@unal.edu.co>
2020-04-10 11:39:23 -0500

    task2 segun oscar

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,9 +13,6 @@ def do_deploy(archive_path):
     """
         using fabric to distribute archive
     """
-    if not os.path.exists(archive_path):
-        return False
-
     try:
         stat(archive_path)
     except:
@@ -25,14 +22,14 @@ def do_deploy(archive_path):
         name = name[1].split(".")
         name = name[0]
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{}.tgz -C \
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(name))
+        run("sudo tar -xzf /tmp/{}.tgz -C \
             /data/web_static/releases/{}/".format(name, name))
-        run("rm /tmp/{}.tgz".format(name))
-        run("mv /data/web_static/releases/{}/web_static/* \
+        run("sudo rm /tmp/{}.tgz".format(name))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ \
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ \
             /data/web_static/current".format(name))
         print("New version deployed!")
         return True
